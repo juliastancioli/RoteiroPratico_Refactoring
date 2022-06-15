eb9290db9e4b911699d3cf6bde924cea7c8022cb
juliastancioli <julia.stancioli@users.noreply.github.com>
2022-06-15 15:51:14 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -36,4 +36,16 @@ public class Rental {
         }
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints() { 
+      //Adicionar o trecho de código extraído.
+        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1)
+            {
+                return 2;
+            }
+        else
+        {
+            return 1;
+        }
+   }
 }
