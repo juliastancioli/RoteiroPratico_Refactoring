be3d7ad089b49b406cb7d42ee27aca6db83a9904
juliastancioli <julia.stancioli@users.noreply.github.com>
2022-06-20 14:07:40 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -3,14 +3,7 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        // Adicionar o trecho de código extraído.
-        if((this.getPriceCode()==Movie.NEW_RELEASE)&&daysRented>1)
-        {
-            return 2;
-        }else
-        {
-            return 1;
-        }
+        return 1;
     }
     
 }
